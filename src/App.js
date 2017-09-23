7fce74c4c170853303e1842a99cfad271569f209
Antonio Rodríguez <antoniwan@gmail.com>
2017-09-23 13:18:10 -0400

    Add report route and cointainer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './containers/Home';
+import Report from './containers/Report';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <Router>
         <div>
           <Route exact path="/" component={Home} />
+          <Route exact path="/report-person" component={Report} />
         </div>
       </Router>
     );
---
+++ src/containers/Report.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import Layout from '../layouts/main';
+
+export default () => (
+  <Layout>
+
+    report...
+  </Layout>
+);
