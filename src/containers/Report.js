06e9e84ba95259403377537a15d446ef2b526993
Antonio Rodríguez <antoniwan@gmail.com>
2017-09-23 13:24:08 -0400

    Add intent copy on the Report component

--- src/containers/Report.js
+++ src/containers/Report.js
@@ -5,7 +5,7 @@ import Layout from '../layouts/main';
 
 export default () => (
   <Layout>
-
-    report...
+    <h2>Just upload photos of the person(s) you have located!</h2>
+    <p>We will detect and match any detectable faces and notify any users that try to find them!</p>
   </Layout>
 );
