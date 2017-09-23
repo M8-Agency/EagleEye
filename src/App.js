8a572dfc079a033695c5223155cc4a8d524f3233
Mohamed Soussi <mohameddsoussi@gmail.com>
2017-09-23 15:26:23 -0400

    results, resultscard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Home from './containers/Home';
 import Find from './containers/Find';
+import PhotoUpload from './containers/PhotoUpload';
+import Results from './containers/Results';
 
 class App extends Component {
   render() {
@@ -10,6 +13,8 @@ class App extends Component {
         <div>
           <Route exact path="/" component={Home} />
           <Route exact path="/find" component={Find} />
+          <Route exact path="/find/photo-upload" component={PhotoUpload} />
+          <Route exact path="/find/results" component={Results} />
         </div>
       </Router>
     );
--- src/containers/PhotoUpload.js
+++ src/containers/PhotoUpload.js
@@ -3,3 +3,75 @@ import styled from 'styled-components';
 
 import Layout from '../layouts/main';
 
+import b from '../components/Button';
+
+const ButtonHeight = "50px";
+
+const Button = b(ButtonHeight);
+
+const UploadInput = styled.input`
+  opacity: 0;
+  width: 0;
+  display: none;
+`;
+
+const ButtonLabel = styled.label`
+  height: ${ButtonHeight};
+  text-align: center;
+  border: 2px solid blue;
+  border-radius: 10px;
+  width: 95%;
+  display: block;
+  line-height: 3rem;
+  font-size: 12pt;
+  color: blue;
+  margin: auto;
+`;
+
+const ImageThumbnail = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 5px;
+`;
+
+const ImageWrapper = styled.div`
+  width: 95%;
+  margin: auto;
+  height: 250px;
+  text-align: center;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+`;
+
+class UploadSection extends React.Component {
+  constructor(...args){
+    super(...args);
+  }
+
+  render(){
+    const Wrapper = styled.div`
+      margin: 15px 0;
+    `;
+
+    return(
+      <Wrapper>
+        <ImageWrapper>
+         <ImageThumbnail src="" alt="Thumbnail" />
+        </ImageWrapper>
+        <Wrapper>
+          <ButtonLabel htmlFor="ImageUpload">Upload</ButtonLabel>
+          <UploadInput id="ImageUpload" type="file" accept="image/*" placeholder="Upload"/>
+          <Button>Find</Button>
+        </Wrapper>
+      </Wrapper>
+    )
+  }
+}
+
+export default () => (
+  <Layout>
+    <section>
+      <UploadSection />
+    </section>
+  </Layout>
+)
