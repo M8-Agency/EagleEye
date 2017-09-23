bbb96831c5c336835d4b04630ac9bf0a5ab7227b
Mohamed Soussi <mohameddsoussi@gmail.com>
2017-09-23 12:58:49 -0400

    started on Find

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './containers/Home';
+import Find from './containers/Find';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <Router>
         <div>
           <Route exact path="/" component={Home} />
+          <Route exact path="/find" component={Find} />
         </div>
       </Router>
     );
