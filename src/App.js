e7ae54bad7bbdc1b79c7f32db891263380550680
Antonio Rodríguez <antoniwan@gmail.com>
2017-09-24 12:06:19 -0400

    Enable catch-all route

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './containers/Home';
 import Find from './containers/Find';
-import PhotoUpload from './containers/PhotoUpload';
-import Results from './containers/Results';
 import Report from './containers/Report';
+import Results from './containers/Results';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/report-person" component={Report} />
-          <Route exact path="/find" component={Find} />
-          <Route exact path="/find/photo-upload" component={PhotoUpload} />
-          <Route exact path="/find/results" component={Results} />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/report" component={Report} />
+            <Route exact path="/find" component={Find} />
+            <Route exact path="/find-results" component={Results} />
+            <Route component={Home} />
+          </Switch>
         </div>
       </Router>
     );
